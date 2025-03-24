fa33b0a8921895c746dc2f6d262a8960ded98c12
Vyacheslavvvv <galin0ff@yandex.ru>
2025-03-25 00:54:42 +0300

    some new knwld

---
+++ src/test/java/ru/kata/spring/boot_security/demo/integration/DbExportTest.java
@@ -0,0 +1,14 @@
+package ru.kata.spring.boot_security.demo.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLException;
+
+@SpringBootTest
+public class DbExportTest {
+
+    @Test
+    void exportSchema() throws SQLException {
+    }
+}
